9652468e2af944b31ef0dd83638aa2222c1b7a3d
hassanzhd <hassanzhd2000@gmail.com>
2023-06-18 04:07:07 +0500

    added subtypes MySqlRawTableMetaData, PostgresRawTableMetaData

--- replicaDB/src/Interfaces/RawTableMetaData.ts
+++ replicaDB/src/Interfaces/RawTableMetaData.ts
@@ -1,7 +1,16 @@
 import { RowDataPacket } from "mysql2";
 import { RawColumnMetaData } from "./RawColumnMetaData";
+import { QueryResult } from "pg";
 
-export interface RawTableMetaData extends RowDataPacket {
-    tableName: string;
-    columnMetaData: RawColumnMetaData[];
+export interface RawTableMetaData {
+  tableName: string;
+  columnMetaData: RawColumnMetaData[];
 }
+
+export interface MySqlRawTableMetaData
+  extends RawTableMetaData,
+    RowDataPacket {}
+
+export interface PostgresRawTableMetaData
+  extends RawTableMetaData,
+    QueryResult {}
