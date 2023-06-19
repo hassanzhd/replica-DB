96e7d89a670e15b8c9c9db9a09118d0be2836c3b
hassanzhd <hassanzhd2000@gmail.com>
2023-06-19 11:07:00 +0500

    added addTables and addRecords methods to IDatabaseService

--- replicaDB/src/Interfaces/IDatabaseService.ts
+++ replicaDB/src/Interfaces/IDatabaseService.ts
@@ -1,7 +1,13 @@
+import { TableMetaData } from "../TableMetaData/TableMetaData";
 import { RawTableMetaData } from "./RawTableMetaData";
 
 export interface IDatabaseService {
   getRawTablesMetaData(): Promise<RawTableMetaData[]>;
+  addTables(tableMetaData: TableMetaData[]): Promise<void>;
+  addRecords(
+    sourceService: IDatabaseService,
+    tableMetaData: TableMetaData[]
+  ): Promise<void>;
   findAll(tableName: string): Promise<object[]>;
   destructor(): Promise<void>;
 }
