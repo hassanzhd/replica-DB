1f27f9a3ccd77776bb68baabff6d069aaee5a20f
hassanzhd <hassanzhd2000@gmail.com>
2023-06-19 11:10:29 +0500

    updated main.ts

--- replicaDB/src/main.ts
+++ replicaDB/src/main.ts
@@ -1,21 +1,38 @@
+import { ConfigService } from "./Config/ConfigService";
 import { TableMetaDataMapper } from "./TableMetaData/TableMetaDataMapper";
 import { PostgresService } from "./postgres/PostgresService";
 import { MySqlService } from "./mysql/MySqlService";
+import { IDatabaseService } from "./Interfaces/IDatabaseService";
+
+async function getDatabaseService(
+  targetName: string
+): Promise<IDatabaseService> {
+  let service = undefined;
+  const config = await ConfigService.getConfig(targetName);
+
+  if (config.engine == "postgres") {
+    service = await PostgresService.getInstance(config);
+    return service;
+  }
+
+  service = await MySqlService.getInstance(config);
+  return service;
+}
 
 async function main() {
-  const postgresClient = await PostgresService.getInstance();
-  const mysqlService = await MySqlService.getInstance();
+  const sourceService = await getDatabaseService("source");
+  const destinationService = await getDatabaseService("destination");
 
-  const rawTablesMetaData = await mysqlService.getRawTablesMetaData();
+  const rawTablesMetaData = await sourceService.getRawTablesMetaData();
   const tableMetaData = rawTablesMetaData.map((rawTableMetaData) =>
     TableMetaDataMapper.toDomain(rawTableMetaData)
   );
 
-  await postgresClient.addTables(tableMetaData);
-  await postgresClient.addRecords(mysqlService, tableMetaData);
+  await destinationService.addTables(tableMetaData);
+  await destinationService.addRecords(sourceService, tableMetaData);
 
-  await mysqlService.destructor();
-  await postgresClient.destructor();
+  await destinationService.destructor();
+  await sourceService.destructor();
 }
 
 main();
