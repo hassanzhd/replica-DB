8e714d8782d22a58ddf4594a6398a365086ffa4d
hassanzhd <hassanzhd2000@gmail.com>
2023-07-04 21:28:40 +0500

    integrate ReplicaService in main.ts

--- replicaDB/src/main.ts
+++ replicaDB/src/main.ts
@@ -1,42 +1,12 @@
-import { ConfigService } from "./config/ConfigService";
-import { TableMetaDataMapper } from "./table_metadata/TableMetaDataMapper";
-import { PostgresService } from "./postgres/PostgresService";
-import { MySqlService } from "./mysql/MySqlService";
-import { IDatabaseService } from "./interfaces/IDatabaseService";
+#!/usr/bin/env node
 
-async function getDatabaseService(
-  targetName: string
-): Promise<IDatabaseService> {
-  let service = undefined;
-  const config = await ConfigService.getConfig(targetName);
-
-  if (config.engine == "postgres") {
-    service = await PostgresService.getInstance(config);
-    return service;
-  }
-
-  service = await MySqlService.getInstance(config);
-  return service;
-}
+import { ReplicaService } from "./replica_service/ReplicaService";
 
 async function main() {
-  const sourceService = await getDatabaseService("source");
-  const destinationService = await getDatabaseService("destination");
-
-  const rawTablesMetaData = await sourceService.getRawTablesMetaData();
-  const tableMetaData = rawTablesMetaData.map((rawTableMetaData) => {
-    const tableDataMapper = new TableMetaDataMapper(
-      sourceService,
-      destinationService
-    );
-    return tableDataMapper.toDomain(rawTableMetaData);
-  });
-
-  await destinationService.addTables(tableMetaData);
-  await destinationService.addRecords(sourceService, tableMetaData);
-
-  await destinationService.destructor();
-  await sourceService.destructor();
+  const service = ReplicaService.getInstance();
+  await service.replicate();
 }
 
-main();
+if (require.main == module) {
+  main();
+}
